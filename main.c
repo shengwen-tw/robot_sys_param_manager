b1b3e1174e6dba8387c07107da6939c8a844c468
Shengwen Cheng <c.shengwen.tw@gmail.com>
2020-05-08 13:11:14 +0800

    print list size and fix naming conflicting issue

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ enum {
 	DEF_SYS_PARAM_ID(TEST_FLOAT)
 } PARAM_ID;
 
-sys_param_data sys_param_list[] = {
+sys_param_data my_sys_param_list[] = {
 	SYS_PARAM_ATTR("test u8", SYS_PARAM_U8),
 	SYS_PARAM_ATTR("test s8", SYS_PARAM_S8),
 	SYS_PARAM_ATTR("test u16", SYS_PARAM_U16),
@@ -24,7 +24,11 @@ sys_param_data sys_param_list[] = {
 void print_sys_param_list(void)
 {
 	int i;
-	for(i = 0; i < get_sys_param_list_size(); i++) {
+	int list_size = get_sys_param_list_size();
+	
+	printf("list size: %d\n\r", list_size);
+
+	for(i = 0; i < list_size; i++) {
 		char *name;
 		char *type_s;
 		int type;
@@ -43,37 +47,37 @@ void print_sys_param_list(void)
 		case SYS_PARAM_U8:
 			type_s = SYS_PARAM_TYPE_TO_STRING(SYS_PARAM_U8);
 			get_sys_param_u8(i, &u8);
-			printf("#%d - name:\"%s\" - type:%s - val:%u\n\r", i, name, type_s, u8);
+			printf("#%d - name:\"%s\" - type:\"%s\" - val:%u\n\r", i, name, type_s, u8);
 			break;
 		case SYS_PARAM_S8:
 			type_s = SYS_PARAM_TYPE_TO_STRING(SYS_PARAM_S8);
 			get_sys_param_s8(i, &s8);
-			printf("#%d - name:\"%s\" - type:%s - val:%d\n\r", i, name, type_s, s8);
+			printf("#%d - name:\"%s\" - type:\"%s\" - val:%d\n\r", i, name, type_s, s8);
 			break;
 		case SYS_PARAM_U16:
 			type_s = SYS_PARAM_TYPE_TO_STRING(SYS_PARAM_U16);
 			get_sys_param_u16(i, &u16);
-			printf("#%d - name:\"%s\" - type:%s - val:%u\n\r", i, name, type_s, u16);
+			printf("#%d - name:\"%s\" - type:\"%s\" - val:%u\n\r", i, name, type_s, u16);
 			break;
 		case SYS_PARAM_S16:
 			type_s = SYS_PARAM_TYPE_TO_STRING(SYS_PARAM_S16);
 			get_sys_param_s16(i, &s16);
-			printf("#%d - name:\"%s\" - type:%s - val:%d\n\r", i, name, type_s, s16);
+			printf("#%d - name:\"%s\" - type:\"%s\" - val:%d\n\r", i, name, type_s, s16);
 			break;
 		case SYS_PARAM_U32:
 			type_s = SYS_PARAM_TYPE_TO_STRING(SYS_PARAM_U32);
 			get_sys_param_u32(i, &u32);
-			printf("#%d - name:\"%s\" - type:%s - val:%u\n\r", i, name, type_s, u32);
+			printf("#%d - name:\"%s\" - type:\"%s\" - val:%u\n\r", i, name, type_s, u32);
 			break;
 		case SYS_PARAM_S32:
 			type_s = SYS_PARAM_TYPE_TO_STRING(SYS_PARAM_S32);
 			get_sys_param_s32(i, &s32);
-			printf("#%d - name:\"%s\" - type:%s - val:%d\n\r", i, name, type_s, s32);
+			printf("#%d - name:\"%s\" - type:\"%s\" - val:%d\n\r", i, name, type_s, s32);
 			break;
 		case SYS_PARAM_FLOAT:
 			type_s = SYS_PARAM_TYPE_TO_STRING(SYS_PARAM_FLOAT);
 			get_sys_param_float(i, &f);
-			printf("#%d - name:\"%s\" - type:%s - val:%f\n\r", i, name, type_s, f);
+			printf("#%d - name:\"%s\" - type:\"%s\" - val:%f\n\r", i, name, type_s, f);
 			break;
 		}
 	}
@@ -81,7 +85,7 @@ void print_sys_param_list(void)
 
 int main(void)
 {
-	init_sys_param_list(sys_param_list, SIZE_OF_PARAM_LIST(sys_param_list));
+	init_sys_param_list(my_sys_param_list, SIZE_OF_PARAM_LIST(my_sys_param_list));
 
 	set_sys_param_u8(TEST_U8, UINT8_MAX);
 	set_sys_param_s8(TEST_S8, INT8_MAX);
